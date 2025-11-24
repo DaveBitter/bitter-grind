dffb7ef0d01311e1dc2979760466d5e7b99a9ade
Dave Bitter <daveybitter@gmail.com>
2025-11-24 16:19:58 +0100

    Update coffee slider to use orange-500 (#f97316) matching data visualization
    - Change slider colors from orange-800/900 to orange-500
    - Ensures consistent orange color scheme across all UI elements
    - Matches the exact orange used in charts (#f97316)

--- src/components/RatioCalculator.tsx
+++ src/components/RatioCalculator.tsx
@@ -143,7 +143,7 @@ export function RatioCalculator({ calculatorState, onCalculatorStateChange, onNa
               </div>
               <span className="text-2xl font-bold tabular-nums">{coffeeAmount}g</span>
             </div>
-            <div className="py-4 [&_.slider-track]:bg-orange-900/20 dark:[&_.slider-track]:bg-orange-700/20 [&_.slider-range]:bg-orange-800 dark:[&_.slider-range]:bg-orange-700 [&_.slider-thumb]:border-orange-800 dark:[&_.slider-thumb]:border-orange-700">
+            <div className="py-4 [&_.slider-track]:bg-orange-500/20 dark:[&_.slider-track]:bg-orange-500/20 [&_.slider-range]:bg-orange-500 dark:[&_.slider-range]:bg-orange-500 [&_.slider-thumb]:border-orange-500 dark:[&_.slider-thumb]:border-orange-500">
               <Slider
                 value={[coffeeAmount]}
                 onValueChange={(vals) => setCoffeeAmount(vals[0])}
